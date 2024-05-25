ae979508d9e03b74faeee5e583ca9067a5695721
yd-tw <yd960528@gmail.com>
2024-05-25 12:34:44 +0800

    Connect to firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 'use client';
 import { signOut, useSession } from 'next-auth/react';
 
+import AddItem from './AddItem';
+
 export default function Home() {
   const session = useSession();
   return (
     <>
       <div >{session?.data?.user?.name }</div>
       <button onClick={() => signOut()}>登入</button>
+      <div>
+      <h1>Welcome to My Next.js App</h1>
+      <AddItem />
+    </div>
     </>
   )
 }
