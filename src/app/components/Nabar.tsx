e01aa807a26541486ed2e97d0a504d471b8c1d96
yd-tw <yd960528@gmail.com>
2024-05-25 15:21:36 +0800

    Move user info to nabar

--- src/app/components/Nabar.tsx
+++ src/app/components/Nabar.tsx
@@ -1,52 +1,57 @@
 "use client";
 import React, { useState } from "react";
+import { signOut, useSession } from 'next-auth/react';
 import { Bars3Icon } from '@heroicons/react/24/outline'
 
 const navigation = [
     { name: '場地列表', href: '#' },
     { name: '申請場地', href: '#' },
     { name: '場地審核', href: '#' },
-  ]
+]
 
 const Navbar = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+    const session = useSession();
 
-  return (
-    <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-    <div className="flex lg:flex-1">
-      <a href="#" className="-m-1.5 p-1.5">
-        <span className="sr-only">Your Company</span>
-        <img
-          className="h-8 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt=""
-        />
-      </a>
-    </div>
-    <div className="flex lg:hidden">
-      <button
-        type="button"
-        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-        onClick={() => setMobileMenuOpen(true)}
-      >
-        <span className="sr-only">Open main menu</span>
-        <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-      </button>
-    </div>
-    <div className="hidden lg:flex lg:gap-x-12">
-      {navigation.map((item) => (
-        <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
-          {item.name}
-        </a>
-      ))}
-    </div>
-    <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-      <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-        登入 <span aria-hidden="true">&rarr;</span>
-      </a>
-    </div>
-  </nav>
-  );
+    return (
+        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+            <div className="flex lg:flex-1">
+                <a href="#" className="-m-1.5 p-1.5">
+                    <span className="sr-only">Your Company</span>
+                    <img
+                        className="h-8 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        alt=""
+                    />
+                </a>
+            </div>
+            <div className="flex lg:hidden">
+                <button
+                    type="button"
+                    className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                    onClick={() => setMobileMenuOpen(true)}
+                >
+                    <span className="sr-only">Open main menu</span>
+                    <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                </button>
+            </div>
+            <div className="hidden lg:flex lg:gap-x-12">
+                {navigation.map((item) => (
+                    <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+                        {item.name}
+                    </a>
+                ))}
+            </div>
+            <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                <div >{session?.data?.user?.name}</div>
+                <div >{session?.data?.user?.email}</div>
+                <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                <button onClick={() => signOut()}>登出</button>
+                </a>
+                
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
