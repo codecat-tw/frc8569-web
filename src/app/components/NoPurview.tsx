42bf6a59a403332091fc21d579e0ca9cbbf3d60d
yd-tw <yd960528@gmail.com>
2024-05-29 23:01:22 +0800

    Update Nopurview page style

--- src/app/components/NoPurview.tsx
+++ src/app/components/NoPurview.tsx
@@ -1,7 +1,10 @@
 export default function NoPurview() {
     return (
-        <div className="bg-white">
-            <h1>權限不足或系統配置錯誤。若您認為設置有誤請聯繫管理團隊。</h1>
+        <div className="flex items-center justify-center min-h-screen bg-blue-100">
+            <div className="text-center p-4">
+                <h1 className="text-3xl font-bold mb-4">糟糕，你好像不能看</h1>
+                <h1>因權限不足或系統配置錯誤導致無法訪問。若您認為設置有誤請聯繫管理團隊。</h1>
+            </div>
         </div>
     )
 }
