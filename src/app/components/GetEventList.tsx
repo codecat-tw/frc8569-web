791d2e96bbc022afee3f387226f256be8285aab1
yd-tw <yd960528@gmail.com>
2024-05-30 19:39:05 +0800

    Split Getlist

---
+++ src/app/components/GetEventList.tsx
@@ -0,0 +1,33 @@
+// GetEventList.tsx
+import { useEffect, useState } from "react";
+import db from "../../utils/firestore";
+import { collection, getDocs } from "firebase/firestore";
+
+interface Item {
+  id: string;
+  date: string;
+  name: string;
+  start: string;
+  end: string;
+  area: string;
+  apply: string;
+  teacher: string;
+  status: string;
+}
+
+const GetEventList = () => {
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const querySnapshot = await getDocs(collection(db, "activity"));
+      setItems(querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id } as Item)));
+    };
+
+    fetchItems();
+  }, []);
+
+  return items;
+};
+
+export default GetEventList;
--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,11 +1,14 @@
 'use client';
-import { useSession } from 'next-auth/react';
-import ShowList from "../components/ShowList";
-import NoPurview from "../components/NoPurview";
 
-export default function List() {
-  const session = useSession();
-  const userEmail = session?.data?.user?.email || 'ErrorUser';
+import { useSession } from 'next-auth/react';
+import GetEventList from "../components/GetEventList";
+import NoPurview from "../components/NoPurview";
+import JoinButton from '../components/JoinButton';
+
+const List: React.FC = () => {
+  const { data: session } = useSession();
+  const items = GetEventList();
+  const userEmail = session?.user?.email || 'ErrorUser';
 
   if (!session || !userEmail.endsWith('@mail2.chshs.ntpc.edu.tw')) {
     return <NoPurview />;
@@ -14,7 +17,25 @@ export default function List() {
   return (
     <div className='min-h-screen bg-blue-100'>
       <h1>活動清單</h1>
-      <ShowList userEmail={userEmail}/>
+      <div className="border w-96 text-center p-4">
+        <ul>
+          {items.map((item) => (
+            <li key={item.id} className="border-t-2 p-2">
+              <p>活動日期: {item.date}</p>
+              <p>活動名稱: {item.name}</p>
+              <p>開始時間: {item.start}</p>
+              <p>結束時間: {item.end}</p>
+              <p>使用分區: {item.area}</p>
+              <p>活動代表: {item.apply}</p>
+              <p>指導老師: {item.teacher}</p>
+              <p>場地狀態: {item.status}</p>
+              <JoinButton id={item.id} userEmail={userEmail} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
+
+export default List;
