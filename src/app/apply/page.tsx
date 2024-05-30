14db1bee5ee6969b9d77ca1e38b078ed1a410122
yd-tw <yd960528@gmail.com>
2024-05-30 22:36:23 +0800

    Update apply page style

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -38,79 +38,89 @@ const AddItemForm: React.FC<{ userEmail: string }> = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>活動日期</label>
-        <input
-          type="date"
-          name="date"
-          value={formValues.date}
-          onChange={handleInputChange}
-          placeholder="Date"
-          required
-        />
-      </div>
-      <div>
-        <label>活動名稱</label>
-        <input
-          type="text"
-          name="name"
-          value={formValues.name}
-          onChange={handleInputChange}
-          placeholder="Name"
-          required
-        />
-      </div>
-      <div>
-        <label>開始時間</label>
-        <input
-          type="time"
-          name="start"
-          value={formValues.start}
-          onChange={handleInputChange}
-          placeholder="Start"
-          required
-        />
-      </div>
-      <div>
-        <label>結束時間</label>
-        <input
-          type="time"
-          name="end"
-          value={formValues.end}
-          onChange={handleInputChange}
-          placeholder="End"
-          required
-        />
-      </div>
-      <div>
-        <label>使用分區</label>
-        <input
-          type="text"
-          name="area"
-          value={formValues.area}
-          onChange={handleInputChange}
-          placeholder="Area"
-          required
-        />
-      </div>
-      <div>
-        <label>指導老師</label>
-        <select
-          name="teacher"
-          value={formValues.teacher}
-          onChange={handleInputChange}
-          required
-        >
-          <option value="" disabled>選擇一個老師</option>
-          <option value="kkbike">華光永</option>
-          <option value="crispin">周長益</option>
-          <option value="angella">李淑卿</option>
-          <option value="eric29433453@gmail.com">系統測試</option>
-        </select>
-      </div>
-      <button type="submit">提交申請</button>
-    </form>
+    <div className='min-h-screen bg-blue-100 overflow-x-hidden p-4'>
+      <form onSubmit={handleSubmit} className="max-w-lg mx-auto bg-white mt-8 p-8 shadow-md rounded-md">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">活動日期</label>
+          <input
+            type="date"
+            name="date"
+            value={formValues.date}
+            onChange={handleInputChange}
+            className="w-full p-2 border border-gray-300 rounded-md"
+            placeholder="Date"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">活動名稱</label>
+          <input
+            type="text"
+            name="name"
+            value={formValues.name}
+            onChange={handleInputChange}
+            className="w-full p-2 border border-gray-300 rounded-md"
+            placeholder="主題、用途等..."
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">開始時間</label>
+          <input
+            type="time"
+            name="start"
+            value={formValues.start}
+            onChange={handleInputChange}
+            className="w-full p-2 border border-gray-300 rounded-md"
+            placeholder="Start"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">結束時間</label>
+          <input
+            type="time"
+            name="end"
+            value={formValues.end}
+            onChange={handleInputChange}
+            className="w-full p-2 border border-gray-300 rounded-md"
+            placeholder="End"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">使用區域</label>
+          <input
+            type="text"
+            name="area"
+            value={formValues.area}
+            onChange={handleInputChange}
+            className="w-full p-2 border border-gray-300 rounded-md"
+            placeholder="填入分區，如: A、B、C、D、E"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">指導老師</label>
+          <select
+            name="teacher"
+            value={formValues.teacher}
+            onChange={handleInputChange}
+            className="w-full p-2 border border-gray-300 rounded-md"
+            required
+          >
+            <option value="" disabled>選擇一個老師</option>
+            <option value="kkbike">華光永</option>
+            <option value="crispin">周長益</option>
+            <option value="angella">李淑卿</option>
+            <option value="eric29433453@gmail.com">系統測試</option>
+          </select>
+        </div>
+        <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600">
+          提交申請
+        </button>
+      </form>
+    </div>
   );
 };
 
