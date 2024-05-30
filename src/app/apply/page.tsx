3d118fcbff48defb6b98ca14d9deb899ef54c049
yd-tw <yd960528@gmail.com>
2024-05-30 22:20:40 +0800

    Split apply comp

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -1,19 +1,117 @@
 'use client';
 import { useSession } from 'next-auth/react';
-import ApplyItem from '../components/ApplyItem'
-import NoPurview from "../components/NoPurview";
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import ApplyItem from '../components/ApplyItem';
 
-export default function Apply() {
-  const session = useSession();
-  const userEmail = session?.data?.user?.email || 'user@error.com';
+const AddItemForm: React.FC<{ userEmail: string }> = () => {
+  const { data: session } = useSession();
+  const userEmail = session?.user?.email || 'ErrorUser';
 
-  if (!session || !userEmail.endsWith('@mail2.chshs.ntpc.edu.tw')) {
-    return <NoPurview />;
-  }
+  const [formValues, setFormValues] = useState({
+    date: '',
+    name: '',
+    start: '',
+    end: '',
+    area: '',
+    teacher: ''
+  });
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await ApplyItem({ formValues, userEmail });
+    setFormValues({
+      date: '',
+      name: '',
+      start: '',
+      end: '',
+      area: '',
+      teacher: ''
+    });
+  };
 
   return (
-    <div className='min-h-screen bg-blue-100'>
-      <ApplyItem userEmail={userEmail} />
-    </div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>活動日期</label>
+        <input
+          type="date"
+          name="date"
+          value={formValues.date}
+          onChange={handleInputChange}
+          placeholder="Date"
+          required
+        />
+      </div>
+      <div>
+        <label>活動名稱</label>
+        <input
+          type="text"
+          name="name"
+          value={formValues.name}
+          onChange={handleInputChange}
+          placeholder="Name"
+          required
+        />
+      </div>
+      <div>
+        <label>開始時間</label>
+        <input
+          type="time"
+          name="start"
+          value={formValues.start}
+          onChange={handleInputChange}
+          placeholder="Start"
+          required
+        />
+      </div>
+      <div>
+        <label>結束時間</label>
+        <input
+          type="time"
+          name="end"
+          value={formValues.end}
+          onChange={handleInputChange}
+          placeholder="End"
+          required
+        />
+      </div>
+      <div>
+        <label>使用分區</label>
+        <input
+          type="text"
+          name="area"
+          value={formValues.area}
+          onChange={handleInputChange}
+          placeholder="Area"
+          required
+        />
+      </div>
+      <div>
+        <label>指導老師</label>
+        <select
+          name="teacher"
+          value={formValues.teacher}
+          onChange={handleInputChange}
+          required
+        >
+          <option value="" disabled>選擇一個老師</option>
+          <option value="kkbike">華光永</option>
+          <option value="crispin">周長益</option>
+          <option value="angella">李淑卿</option>
+          <option value="eric29433453@gmail.com">系統測試</option>
+        </select>
+      </div>
+      <button type="submit">提交申請</button>
+    </form>
+  );
+};
+
+export default AddItemForm;
---
+++ src/app/components/ApplyItem.tsx
@@ -0,0 +1,37 @@
+import { collection, setDoc, doc } from "firebase/firestore";
+import db from '../../utils/firestore';
+
+interface FormValues {
+    date: string;
+    name: string;
+    start: string;
+    end: string;
+    area: string;
+    teacher: string;
+}
+
+interface AddItemProps {
+    formValues: FormValues;
+    userEmail: string;
+}
+
+const ApplyItem = async ({ formValues, userEmail }: AddItemProps) => {
+    const { date, name, start, end, area, teacher } = formValues;
+    if (date && name && start && end && area && teacher) {
+        try {
+            const docRef = doc(collection(db, "activity"), date);
+            await setDoc(docRef, {
+                ...formValues,
+                apply: userEmail,
+                status: "尚未審核",
+            });
+            console.log("Document written with ID: ", docRef.id);
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+    } else {
+        console.log("All fields are required.");
+    }
+};
+
+export default ApplyItem;
