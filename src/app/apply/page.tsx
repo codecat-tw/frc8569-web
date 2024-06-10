c0ec5a2ad99abb5ea29f03e7e24e226921f5c9a7
yd-tw <yd960528@gmail.com>
2024-06-10 15:36:41 +0800

    Add auto join url and line open use chrome

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -60,7 +60,7 @@ const AddItemForm: React.FC = () => {
   };
 
   const handleCopyUrl = () => {
-    navigator.clipboard.writeText("frc.kuang-ti.com/events/" + activityUrl );
+    navigator.clipboard.writeText("frc.kuang-ti.com/events/" + activityUrl + "?join=1&openExternalBrowser=1");
     alert('活動網址已複製到剪貼簿');
   };
 
