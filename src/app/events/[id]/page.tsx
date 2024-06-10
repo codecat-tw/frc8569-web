149790ae6df8687d12afa6dc6b59bd5ddd96c9b2
yd-tw <yd960528@gmail.com>
2024-06-10 16:04:20 +0800

    Fix join function use erroruser

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -23,7 +23,6 @@ const EventsPage = () => {
   const params = useParams() || {};
   const searchParams = useSearchParams()
   const [eventData, setEventData] = useState<Item | null>(null);
-  const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [joinExecuted, setJoinExecuted] = useState(false);
   const [resultMessage, setResultMessage] = useState<string | null>(null); // 新增狀態變量
@@ -35,7 +34,6 @@ const EventsPage = () => {
     console.log("fetchEventData useEffect");
     const fetchEventData = async () => {
       if (!params.id || Array.isArray(params.id)) {
-        setLoading(false);
         setError('無效的參數');
         return;
       }
@@ -54,13 +52,11 @@ const EventsPage = () => {
       } catch (error) {
         console.error("Error fetching document: ", error);
         setError('獲取資料時出錯');
-      } finally {
-        setLoading(false);
       }
     };
 
     fetchEventData();
-  }, []);
+  }, [params.id]);
 
   useEffect(() => {
     console.log("executeJoinEvent useEffect");
@@ -77,20 +73,18 @@ const EventsPage = () => {
 
           const decodedId = decodeURIComponent(params.id);
           console.log("Calling joinEvent with ID:", decodedId);
-          const resultMessage = await joinEvent(decodedId, userEmail, userName);
-          console.log("joinEvent result:", resultMessage);
-          setResultMessage(resultMessage); // 設定 resultMessage 狀態變量
-          setJoinExecuted(true);
+          if (userEmail !== 'ErrorUser' && userName !== 'ErrorUser') {
+            const resultMessage = await joinEvent(decodedId, userEmail, userName);
+            console.log("joinEvent result:", resultMessage);
+            setResultMessage(resultMessage);
+            setJoinExecuted(true);
+          }
         }
       }
     };
 
     executeJoinEvent();
   }, [userEmail, userName]);
-
-  if (loading) {
-    return <div className="min-h-screen bg-blue-100 flex items-center justify-center">載入中...</div>;
-  }
 
   if (error) {
     return <div className="min-h-screen bg-blue-100 flex items-center justify-center">{error}</div>;
