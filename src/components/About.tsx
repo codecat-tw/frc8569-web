8ff357f6ee65ee6886a2ec2a54e29d802867cdca
yd-tw <yd960528@gmail.com>
2024-06-11 22:06:48 +0800

    Update Home info

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,8 @@ export default function Home() {
     <div className="text-black flex items-center justify-center min-h-screen bg-blue-100">
       <div className="text-center p-4">
         <h1 className="text-3xl font-bold mb-4">歡迎來到FRC管理系統</h1>
-        <p className="text-lg mb-4">網站穩定性測試中...</p>
+        <p className="text-lg mb-4">整合申請、報名、管理、統計於一體網站</p>
+        <p className="text-lg mb-4">支援連結自動報名、多功能管理</p>
         <p className="text-lg mb-4">遇到問題請聯繫開發者楊光地</p>
         <p className="text-lg mb-4">郵件請洽: 110330@mail2.chshs.ntpc.edu.tw</p>
         <div className="flex flex-col space-y-4">
