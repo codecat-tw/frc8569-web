842bc7d04ea526bedb6d0e012cd2a38e97c082eb
yd-tw <yd960528@gmail.com>
2024-08-22 20:15:13 +0800

    Update home comp

--- src/components/home/ActivityCard.tsx
+++ src/components/home/ActivityCard.tsx
@@ -1,27 +1,22 @@
-import Link from 'next/link';
-import Image from 'next/image'
+import { Icon } from '@iconify/react';
 
 export default function About() {
   return (
-    <div className="container mx-auto flex flex-col">
-      <div className="flex flex-col md:flex-row items-center bg-blue-100 p-8">
-        <div className="md:w-1/2 flex justify-center items-center">
-          <Image
-            className="w-auto cursor-pointer"
-            src="/FRC.jpg"
-            alt="FRC Icon"
-            width={256}
-            height={256}
-          />
+    <div className="container mx-auto flex flex-col px-4 sm:px-8">
+      <div className="flex flex-col md:flex-row items-center bg-blue-100 p-4 sm:p-8">
+        <div className="w-full md:w-1/2 flex justify-center items-center p-4">
+          <Icon icon="ic:round-format-shapes" width="128" height="128" />
         </div>
-        <div className="md:w-1/2 mt-8 md:mt-0 md:ml-4">
-          <h2 className="mb-4 text-3xl font-bold text-gray-800">活動報名</h2>
-          <p className="mb-6 text-gray-600">
+        <div className="w-full md:w-1/2 mt-4 md:mt-0 md:ml-4">
+          <h2 className="mb-2 text-2xl sm:text-3xl font-bold text-gray-800">活動報名</h2>
+          <p className="mb-4 text-sm sm:text-base text-gray-600">
             系統提供完整的報名與管理系統，並支援「連結自動報名」，使成員報名流程更加簡便。對於管理者而言，後台的審核機制也十分完善，一目了然即可知道活動狀況！
           </p>
-          <button className="py-2 px-4 text-white font-semibold bg-blue-500 rounded hover:bg-blue-600 transition duration-300">
-            開始使用
-          </button>
+          <div className="flex justify-center md:justify-start">
+            <button className="py-3 px-5 sm:py-2 sm:px-4 text-white font-semibold bg-blue-500 rounded hover:bg-blue-600 transition duration-300">
+              開始使用
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/home/HelpCard.tsx
+++ src/components/home/HelpCard.tsx
@@ -1,27 +1,22 @@
-import Link from 'next/link';
-import Image from 'next/image'
+import { Icon } from '@iconify/react';
 
 export default function About() {
   return (
-    <div className="container mx-auto flex flex-col">
-      <div className="flex flex-col md:flex-row items-center bg-blue-100 p-8">
-        <div className="md:w-1/2 flex justify-center items-center">
-          <Image
-            className="w-auto cursor-pointer"
-            src="/FRC.jpg"
-            alt="FRC Icon"
-            width={256}
-            height={256}
-          />
+    <div className="container mx-auto flex flex-col px-4 sm:px-8">
+      <div className="flex flex-col md:flex-row items-center bg-blue-100 p-4 sm:p-8">
+        <div className="w-full md:w-1/2 flex justify-center items-center p-4">
+        <Icon icon="ic:twotone-phone-forwarded" width="128" height="128" />
         </div>
-        <div className="md:w-1/2 mt-8 md:mt-0 md:ml-4">
-          <h2 className="mb-4 text-3xl font-bold text-gray-800">客服支援</h2>
-          <p className="mb-6 text-gray-600">
-            遇到問題或是想提供建議嗎？此網站由楊光地與程式貓團隊協同開發，歡迎隨時聯繫。
+        <div className="w-full md:w-1/2 mt-4 md:mt-0 md:ml-4">
+          <h2 className="mb-2 text-2xl sm:text-3xl font-bold text-gray-800">客服支援</h2>
+          <p className="mb-4 text-sm sm:text-base text-gray-600">
+          遇到問題或是想提供建議嗎？此網站由楊光地與程式貓團隊協同開發，歡迎隨時聯繫。
           </p>
-          <button className="py-2 px-4 text-white font-semibold bg-blue-500 rounded hover:bg-blue-600 transition duration-300">
-            開始使用
-          </button>
+          <div className="flex justify-center md:justify-start">
+            <button className="py-3 px-5 sm:py-2 sm:px-4 text-white font-semibold bg-blue-500 rounded hover:bg-blue-600 transition duration-300">
+              開始使用
+            </button>
+          </div>
         </div>
       </div>
     </div>
