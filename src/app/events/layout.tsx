c8138a6f191f3b31d665333d7f62ad437ca66b51
yd-tw <yd960528@gmail.com>
2024-08-28 23:48:28 +0800

    Add page metadate

---
+++ src/app/events/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "場地列表 | FRC8569",
+  description: "完整列出過去中和高中FRC場地借用紀錄",
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
