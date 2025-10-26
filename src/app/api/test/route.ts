283d5eda1a91f27a7aaea549f352033781e4c00a
yd-tw <yd960528@gmail.com>
2025-10-26 14:13:36 +0800

    add test api

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+import { db } from '@/lib/mongodb';
+import { User } from '@/types/user';
+
+export async function GET() {
+  try {
+    const users = await db.collection<User>('user').find({}).limit(10).toArray();
+
+    return NextResponse.json(users);
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ error: 'Unable to fetch users' }, { status: 500 });
+  }
+}
